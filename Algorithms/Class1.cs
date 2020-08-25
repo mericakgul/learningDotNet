76128a09458b4902ea40bc1241be49dd22fcd404
mericakgul <makgul@worth.systems>
2020-08-25 16:25:14 +0300

    initial commit

---
+++ Algorithms/Class1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Algorithms
+{
+    public class Algorithm
+    {
+        public List <int> createListOfPrimeNumbers(int N){
+
+            List <int> primeNumbers = new List<int> ();
+
+            if (N <= 1){
+                return primeNumbers;
+            }
+
+            else if(N==2){
+                primeNumbers.Add(2);
+                return primeNumbers;
+            }
+
+            else{
+                primeNumbers.Add(2);
+                for(int num = 3 ; num <= N ; num++){
+                    Boolean control = true;
+                    int i = 2;
+
+                    do{ 
+                        if (num%i == 0){
+                            control = false;
+                            break;
+                        }
+                        i++;
+                    }while(i <= num/2);
+
+                    if(control == true){
+                        primeNumbers.Add(num);
+                    }
+                }
+            return primeNumbers;
+            }
+        }
+    
+        public List <int> createListOfFibonacci(int N){
+
+             List <int> fibonacciSequence = new List<int> ();
+             int n1 = 0;
+             int n2 = 1;
+             int n3;
+            
+            if(N < 0){
+                return fibonacciSequence;
+            }
+
+            else if(N == 0){
+                fibonacciSequence.Add(n1);
+                return fibonacciSequence;
+            }
+
+            else{
+                fibonacciSequence.Add(n1);
+                fibonacciSequence.Add(n2);
+                while(n1+n2 <= N){
+                    n3 = n1 + n2;
+                    fibonacciSequence.Add(n3);
+                    n1 = n2;
+                    n2 = n3;
+                }
+                return fibonacciSequence;
+            }
+        }
+    }
+}
---
+++ UnitTests/UnitTest1.cs
@@ -0,0 +1,126 @@
+using System;
+using Xunit;
+using System.Collections.Generic;
+using Algorithms;
+
+namespace UnitTests
+{
+    public class UnitTest1
+    {
+        Algorithm myObj = new Algorithm();
+        List <int> listOfPrimeNumbers = new List<int>();
+        List <int> listOfFibonacciSequence = new List<int>();
+
+        [Fact]
+        public void PrimeTest1()
+        {
+            listOfPrimeNumbers = myObj.createListOfPrimeNumbers(-1);
+            int size = listOfPrimeNumbers.Count;
+            Assert.StrictEqual(0, size);
+        }
+        [Fact]
+        public void PrimeTest2()
+        {
+            listOfPrimeNumbers = myObj.createListOfPrimeNumbers(0);
+            int size = listOfPrimeNumbers.Count;
+            Assert.StrictEqual(0, size);
+        }
+        [Fact]
+        public void PrimeTest3()
+        {
+            listOfPrimeNumbers = myObj.createListOfPrimeNumbers(1);
+            int size = listOfPrimeNumbers.Count;
+            Assert.StrictEqual(0, size);
+        }
+        [Fact]
+        public void PrimeTest4()
+        {
+            listOfPrimeNumbers = myObj.createListOfPrimeNumbers(2);
+            int size = listOfPrimeNumbers.Count;
+            Assert.StrictEqual(1, size);
+            Assert.StrictEqual(2, listOfPrimeNumbers[0]);
+        }
+        [Fact]
+        public void PrimeTest5()
+        {
+            listOfPrimeNumbers = myObj.createListOfPrimeNumbers(10);
+            int size = listOfPrimeNumbers.Count;
+            Assert.StrictEqual(4, size);
+            Assert.StrictEqual(2, listOfPrimeNumbers[0]);
+            Assert.StrictEqual(3, listOfPrimeNumbers[1]);
+            Assert.StrictEqual(5, listOfPrimeNumbers[2]);
+            Assert.StrictEqual(7, listOfPrimeNumbers[3]);
+        }
+        [Fact]
+        public void FibonacciTest1()
+        {
+            listOfFibonacciSequence = myObj.createListOfFibonacci(-1);
+            int size = listOfFibonacciSequence.Count;
+            Assert.StrictEqual(0, size);
+        }
+        [Fact]
+        public void FibonacciTest2()
+        {
+            listOfFibonacciSequence = myObj.createListOfFibonacci(0);
+            int size = listOfFibonacciSequence.Count;
+            Assert.StrictEqual(1, size);
+            Assert.StrictEqual(0, listOfFibonacciSequence[0]);
+        }
+        [Fact]
+        public void FibonacciTest3()
+        {
+            listOfFibonacciSequence = myObj.createListOfFibonacci(1);
+            int size = listOfFibonacciSequence.Count;
+            Assert.StrictEqual(2, size);
+            Assert.StrictEqual(0, listOfFibonacciSequence[0]);
+            Assert.StrictEqual(1, listOfFibonacciSequence[1]);
+        }
+        [Fact]
+        public void FibonacciTest4()
+        {
+            listOfFibonacciSequence = myObj.createListOfFibonacci(2);
+            int size = listOfFibonacciSequence.Count;
+            Assert.StrictEqual(4, size);
+            Assert.StrictEqual(0, listOfFibonacciSequence[0]);
+            Assert.StrictEqual(1, listOfFibonacciSequence[1]);
+            Assert.StrictEqual(1, listOfFibonacciSequence[2]);
+            Assert.StrictEqual(2, listOfFibonacciSequence[3]);
+        }
+        [Fact]
+        public void FibonacciTest5()
+        {
+            listOfFibonacciSequence = myObj.createListOfFibonacci(3);
+            int size = listOfFibonacciSequence.Count;
+            Assert.StrictEqual(5, size);
+            Assert.StrictEqual(0, listOfFibonacciSequence[0]);
+            Assert.StrictEqual(1, listOfFibonacciSequence[1]);
+            Assert.StrictEqual(1, listOfFibonacciSequence[2]);
+            Assert.StrictEqual(2, listOfFibonacciSequence[3]);
+            Assert.StrictEqual(3, listOfFibonacciSequence[4]);
+        }
+        [Fact]
+        public void FibonacciTest6()
+        {
+            listOfFibonacciSequence = myObj.createListOfFibonacci(4);
+            int size = listOfFibonacciSequence.Count;
+            Assert.StrictEqual(5, size);
+            Assert.StrictEqual(0, listOfFibonacciSequence[0]);
+            Assert.StrictEqual(1, listOfFibonacciSequence[1]);
+            Assert.StrictEqual(1, listOfFibonacciSequence[2]);
+            Assert.StrictEqual(2, listOfFibonacciSequence[3]);
+            Assert.StrictEqual(3, listOfFibonacciSequence[4]);
+        }
+        [Fact]
+        public void FibonacciTest7()
+        {
+            listOfFibonacciSequence = myObj.createListOfFibonacci(1000);
+            int size = listOfFibonacciSequence.Count;
+            Assert.StrictEqual(17, size);
+            Assert.StrictEqual(144, listOfFibonacciSequence[12]);
+            Assert.StrictEqual(233, listOfFibonacciSequence[13]);
+            Assert.StrictEqual(377, listOfFibonacciSequence[14]);
+            Assert.StrictEqual(610, listOfFibonacciSequence[15]);
+            Assert.StrictEqual(987, listOfFibonacciSequence[16]);
+        }
+    }
+}
